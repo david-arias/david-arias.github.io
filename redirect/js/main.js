db951b12cedd0fa7eea0ce486246c9389000d5e4
david Arias <devius123@gmail.com>
2014-09-08 18:24:53 -0500

    alerta de version

--- redirect/js/main.js
+++ redirect/js/main.js
@@ -4,16 +4,26 @@ $(document).ready(function() {
 
 	var brw = new Browser();
 
-	alert('fullName: ' + brw.fullName + '\n' + 'name: ' + brw.name + '\n' +
-		'fullVersion: ' + brw.fullVersion + '\n' + 'version: ' + brw.version + '\n' +
-		'platform: ' + brw.platform+ '\n' + 'mobile: ' + brw.mobile+ '\n' +
-		'resolution: ' + brw.width + 'x' + brw.height);
+	alert(
+			'fullName: ' + brw.fullName + '\n' +
+			'name: ' + brw.name + '\n' +
+			'fullVersion: ' + brw.fullVersion + '\n' +
+			'version: ' + brw.version + '\n' +
+			'platform: ' + brw.platform+ '\n' +
+			'mobile: ' + brw.mobile+ '\n' +
+			'resolution: ' + brw.width + 'x' + brw.height
+		);
 
 
 	/*chrome*/
 	if (brw.name === "chrome") {
 		
 		//window.location.replace("http://www.google.com");
+
+		$( ".dataFName" ).text ('fullName: ' + brw.fullName );
+		$( ".dataName" ).text ('name: ' + brw.name );
+		$( ".dataFVer" ).text ('fullVersion: ' + brw.fullVersion );
+		$( ".dataFVer" ).text ('version: ' + brw.version );
 
 	}
 
