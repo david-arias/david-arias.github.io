7a83eac7010cc72ee389341f99d2dd73fef93009
david Arias <devius123@gmail.com>
2014-09-08 18:02:53 -0500

    test redirect

---
+++ redirect/js/main.js
@@ -0,0 +1,21 @@
+/* * * * */
+
+$(document).ready(function() {
+
+	var brw = new Browser();
+
+	/*alert('fullName: ' + brw.fullName + '\n' + 'name: ' + brw.name + '\n' +
+		'fullVersion: ' + brw.fullVersion + '\n' + 'version: ' + brw.version + '\n' +
+		'platform: ' + brw.platform+ '\n' + 'mobile: ' + brw.mobile+ '\n' +
+		'resolution: ' + brw.width + 'x' + brw.height);*/
+
+
+	/*chrome*/
+	if (brw.name === "chrome") {
+		
+		window.location.replace("http://www.google.com");
+
+	}
+
+
+});
