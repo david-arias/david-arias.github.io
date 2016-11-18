ef1ff5d4252d51176bd9e231683852b45b6dfe3b
Dvid Arias <devius123@gmail.com>
2016-11-18 09:10:56 -0500

    renew github io

--- tsb-prod/modules/scripts.php
+++
@@ -1,6 +0,0 @@
-<!-- /* sripts */ -->
-
-<script src="js/jquery.min.js"></script>
-<script src="js/TweenMax.min.js"></script>
-<script src="js/send2.js"></script>
-<script src="js/main.js"></script>
